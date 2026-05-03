a343e2e1a01149375a784b53b899a18d1d55a10e
yumufeixue <18081138300@163.com>
2026-05-03 21:27:22 +0800

    Remove deprecated Config.setFps and Config.setDurationInFrames

--- remotion.config.ts
+++ remotion.config.ts
@@ -3,5 +3,3 @@ import { Config } from '@remotion/cli/config';
 Config.setVideoImageFormat('jpeg');
 Config.setJpegQuality(100);
 Config.setOverwriteOutput(true);
-Config.setFps(30);
-Config.setDurationInFrames(150); // 5秒视频 (30fps * 5s)
